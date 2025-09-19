fe9f2d05bdb80be2ba0e5d8c0c4748aa035d965d
thelissimus <thelissimus@tuta.io>
2025-09-19 12:45:43 +0500

    add: Politeness, Tense & Particle

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,1 +1,22 @@
-module Lib () where
+module Lib
+  ( Politeness (..)
+  , Tense (..)
+  , Particle (..)
+  ) where
+
+data Politeness
+  = Casual
+  | Formal
+
+data Tense
+  = Past
+  | NonPast
+
+data Particle
+  = Wa -- は
+  | Ga -- が
+  | Wo -- を
+  | He -- へ
+  | Ni -- に
+  | De -- で
+  | No -- の
