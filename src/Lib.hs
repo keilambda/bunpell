ab763a5d06f6960048d1f3094cdd97f1f723438a
thelissimus <thelissimus@tuta.io>
2025-09-19 16:28:03 +0500

    add: Sentence

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
   ( Politeness (..)
   , Tense (..)
   , Particle (..)
+  , Word (..)
+  , Role (..)
+  , Sentence (..)
   ) where
+
+import Data.List
+import Data.String (IsString)
+import Data.Text
 
 data Politeness
   = Casual
@@ -20,3 +29,28 @@ data Particle
   | Ni -- に
   | De -- で
   | No -- の
+
+newtype Word = MkWord Text
+  deriving newtype (IsString)
+
+data Role
+  = Subject Word Particle
+  | Topic Word Particle
+  | Object Word
+  | Verb Word
+
+data Sentence = MkSentence
+  { content :: List Role
+  , tense :: Tense
+  , politeness :: Politeness
+  }
+
+foo :: Sentence
+foo = MkSentence{content, tense = NonPast, politeness = Formal}
+ where
+  content =
+    [ Subject "私" No
+    , Topic "名前" Ha
+    , Object "ケイ"
+    , Verb "です"
+    ]
