c897dea31603515f14ec15c5fd2356a35d6f8205
thelissimus <thelissimus@tuta.io>
2025-09-20 23:01:48 +0500

    add: `numtide/devshell`

--- flake.nix
+++ flake.nix
@@ -4,16 +4,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     { self
     , nixpkgs
     , flake-utils
+    , devshell
     }:
     flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs { localSystem = { inherit system; }; };
+      pkgs = import nixpkgs {
+        localSystem = { inherit system; };
+        overlays = [
+          devshell.overlays.default
+        ];
+      };
       hpkgs = pkgs.haskell.packages."ghc912";
 
       bunpell = pkgs.haskell.lib.overrideCabal (hpkgs.callCabal2nix "bunpell" ./. { }) (old: {
@@ -26,8 +34,8 @@
     {
       packages.default = bunpell;
 
-      devShells.default = pkgs.mkShell {
-        buildInputs = [
+      devShells.default = pkgs.devshell.mkShell {
+        packages = [
           hpkgs.cabal-install
           hpkgs.cabal-add
           hpkgs.haskell-language-server
