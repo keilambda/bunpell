19c9d552b68e1efd788f89a5e23966748a4766aa
thelissimus <thelissimus@tuta.io>
2025-09-21 01:34:58 +0500

    fix: `Main.me`

--- app/Main.hs
+++ app/Main.hs
@@ -13,7 +13,7 @@ main = case validate me of
   Success s -> putPrettyLn s
 
 me :: Sentence
-me = MkSentence{content, style = MkStyle{tense = NonPast, formality = Formal}}
+me = MkSentence{content, style = MkStyle{tense = NonPast, formality = Formal, mood = Positive}}
  where
   content =
     [ Subject "私" No
