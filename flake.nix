06b7ac19fb2ab3664dbd25fd4a07f454bb6a177b
thelissimus <thelissimus@tuta.io>
2025-11-15 23:14:52 +0500

    remove: numtide/devshell

--- flake.nix
+++ flake.nix
@@ -4,15 +4,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    devshell.url = "github:numtide/devshell";
-    devshell.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     { self
     , nixpkgs
     , flake-parts
-    , devshell
     }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
@@ -31,46 +28,15 @@
           });
         in
         {
-          _module.args.pkgs = import nixpkgs {
-            localSystem = { inherit system; };
-            overlays = [
-              devshell.overlays.default
-            ];
-          };
-
           packages.default = bunpell;
 
-          devShells.default = pkgs.devshell.mkShell {
-            packages = [
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
               hpkgs.cabal-install
               hpkgs.cabal-add
               hpkgs.haskell-language-server
               hpkgs.fourmolu
               hpkgs.ghc
-            ];
-
-            commands = [
-              {
-                name = "coverage";
-                command = ''
-                  set -euo pipefail
-                  cabal test --enable-coverage --test-show-details=streaming
-                  cabal run bunpell --enable-coverage || true
-                  outdir=coverage/html
-                  rm -rf "$outdir"
-                  mkdir -p "$outdir"
-                  mapfile -t HPCDIRS < <(find dist-newstyle -type d -path '*/hpc/*/mix' | sort -u)
-                  while IFS= read -r TIX; do
-                    comp=$(basename "$TIX" .tix)
-                    dest="$outdir/$comp"
-                    echo "Generating HTML for $comp -> $dest"
-                    ARGS=()
-                    for d in "''${HPCDIRS[@]}"; do ARGS+=("--hpcdir=$d"); done
-                    hpc markup "$TIX" "''${ARGS[@]}" --destdir="$dest" || true
-                  done < <(find dist-newstyle -name '*.tix' | sort)
-                  echo "Coverage HTML written under: $outdir"
-                '';
-              }
             ];
           };
         };
